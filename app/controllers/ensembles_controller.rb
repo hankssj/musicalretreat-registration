954cb3f218ca6ffad589f6dcecf30dc560ca6e4a
Steve Hanks <hanks@pobox.com>
2015-03-02 23:40:22 -0500

    ens

--- app/controllers/ensembles_controller.rb
+++ app/controllers/ensembles_controller.rb
@@ -8,7 +8,7 @@ class EnsemblesController < ApplicationController
     user = User.find(session[:user_id])
     if user && user.has_current_registration
       @registration = user.most_recent_registration
-      unless @registration.instrument
+      unless @registration.instrument_id
         flash[:notice] = "You registered as a non-particpant -- no ensemble choice for you"
         redirect_to :controller => :registration, :action => :index
       end
@@ -17,7 +17,7 @@ class EnsemblesController < ApplicationController
       Event.log("#{user.email} tried to choose ensembles but no registration")
       redirect_to :controller => :registration, :action => "index"
     end
-    @instrument_name = @registration.instrument.display_name
+    @instrument_name = Instrument.find(@registration.instrument_id).display_name
   end
 
 end
