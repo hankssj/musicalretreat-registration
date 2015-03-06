a23d6e3c5b8d0a538e7d049fd412ddef88a6ad97
Steve Hanks <hanks@pobox.com>
2015-03-06 09:25:54 -0500

    development

--- db/migrate/20150305232459_create_ensemble_primaries.rb
+++ db/migrate/20150305232459_create_ensemble_primaries.rb
@@ -1,13 +1,27 @@
+# class CreateEnsemblePrimaries < ActiveRecord::Migration
+#   def change
+#     create_table :ensemble_primaries do |t|
+#       t.int :registration_id
+#       t.int :large_ensemble_choice
+#       t.int :chamber_ensemble_choice
+#       t.boolean :ack_no_morning_ensemble
+#       t.boolean :want_sing_in_chorus
+#       t.boolean :want_percussion_in_band
+
+#       t.timestamps
+#     end
+#   end
+# end
+
 class CreateEnsemblePrimaries < ActiveRecord::Migration
   def change
     create_table :ensemble_primaries do |t|
-      t.int :registration_id
-      t.int :large_ensemble_choice
-      t.int :chamber_ensemble_choice
-      t.boolean :ack_no_morning_ensemble
-      t.boolean :want_sing_in_chorus
-      t.boolean :want_percussion_in_band
-
+      t.column :registration_id, :int
+      t.column :large_ensemble_choice, :int
+      t.column :chamber_ensemble_choice, :int
+      t.column :ack_no_morning_ensemble, :boolean
+      t.column :want_sing_in_chorus, :boolean
+      t.column :want_percussion_in_band, :boolean
       t.timestamps
     end
   end
