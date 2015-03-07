e81c3a6421bc5fd726b802d2afa5e280c79ef512
Steve Hanks <hanks@pobox.com>
2015-03-07 00:20:28 -0500

    reeefactor

--- app/models/ensemble_primary.rb
+++ app/models/ensemble_primary.rb
@@ -1,6 +1,7 @@
 class EnsemblePrimary < ActiveRecord::Base
   belongs_to :registration
-  has_many :mmr_chambers, :preselect_chambers
+  has_many :mmr_chambers
+  has_many :preselect_chambers
   accepts_nested_attributes_for :mmr_chambers
   accepts_nested_attributes_for :preselect_chambers
 end
