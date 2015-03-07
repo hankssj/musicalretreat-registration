4a097d7cee19efc349831d820d5d5c062a72e4bd
Steve Hanks <hanks@pobox.com>
2015-03-07 17:20:12 -0500

    f

--- db/migrate/20150307221439_create_join_table_ensemble_primary_elective.rb
+++ db/migrate/20150307221439_create_join_table_ensemble_primary_elective.rb
@@ -2,8 +2,8 @@ class CreateJoinTableEnsemblePrimaryElective < ActiveRecord::Migration
   def change
     create_join_table :ensemble_primaries, :electives do |t|
       t.integer :rank
-      t.index [:ensemble_primary_id, :elective_id]
-      t.index [:elective_id, :ensemble_primary_id]
+      #t.index [:ensemble_primary_id, :elective_id]
+      #t.index [:elective_id, :ensemble_primary_id]
     end
   end
 end
