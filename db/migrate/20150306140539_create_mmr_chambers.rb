3d8b32b0b212ef7eecc1de24e0649a6955c96710
Steve Hanks <hanks@pobox.com>
2015-03-06 22:56:52 -0500

    reeefactor

--- db/migrate/20150306140539_create_mmr_chambers.rb
+++ db/migrate/20150306140539_create_mmr_chambers.rb
@@ -2,7 +2,7 @@ class CreateMmrChambers < ActiveRecord::Migration
   def change
     create_table :mmr_chambers do |t|
       t.column :ensemble_primary_id, :int
-      t.column :instrument_id_1, :int
+      t.column :instrument_id, :int
       t.column :string_novice, :boolean
       t.column :jazz_ensemble, :boolean
 
