38a4f8bfa0fded7adb90317eb81d612a0ab63dba
Steve Hanks <hanks@pobox.com>
2015-03-07 16:18:34 -0500

    nonempty

--- app/controllers/.#ensembles_controller.rb
+++
@@ -1,1 +0,0 @@
-deploy@node1650.speedyrails.net.22987:1424151458
--- db/migrate/20150305233115_create_preselect_chambers.rb
+++ db/migrate/20150305233115_create_preselect_chambers.rb
@@ -1,29 +1,3 @@
-# class CreatePreselectChambers < ActiveRecord::Migration
-#   def change
-#     create_table :preselect_chambers do |t|
-#       t.int :ensemble_primary_id
-#       t.string :contact_name
-#       t.string :string
-#       t.string :contact_phone
-#       t.string :name_1
-#       t.int :instrument_id_1
-#       t.string :name_2
-#       t.int :instrument_id_2
-#       t.string :name_3
-#       t.int :instrument_id_3
-#       t.string :name_4
-#       t.int :instrument_id_4
-#       t.string :name_5
-#       t.int :instrument_id_5
-#       t.boolean :bring_own_music
-#       t.string :music_name
-#       t.string :music_composer
-
-#       t.timestamps
-#     end
-#   end
-# end
-
 class CreatePrearrangedChambers < ActiveRecord::Migration
   def change
     create_table :preselect_chambers do |t|
