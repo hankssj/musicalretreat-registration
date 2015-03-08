175dad4ede415602733e304a8fa16023ffc6b7f4
Steve Hanks <hanks@pobox.com>
2015-03-08 09:36:35 -0400

    x

--- app/controllers/ensembles_controller.rb
+++ app/controllers/ensembles_controller.rb
@@ -29,7 +29,6 @@ class EnsemblesController < ApplicationController
       raise e
     end
     num_mmr, num_prearranged = parse_chamber_music_choice(@ensemble_primary.chamber_ensemble_choice)
-    Rails.logger.error("XX #{num_mmr} #{num_prearranged}")
     num_mmr.times.each{|i| Rails.logger.error("Create in MMR"); MmrChamber.create!(:ensemble_primary_id => @ensemble_primary.id)}
     num_prearranged.times.each{|i| Rails.logger.error("Create in prearranged");PrearrangedChamber.create!(:ensemble_primary_id => @ensemble_primary.id)}
   end
@@ -71,5 +70,27 @@ class EnsemblesController < ApplicationController
                                              )
 
   end
+
+  def parse_chamber_music_choice(cmc)
+    num_mmr = num_prearranged = 0
+    case cmc
+    when 0
+      num_mmr = 0; num_prearranged = 0
+    when 1
+      num_mmr = 1; num_prearranged = 0
+    when 2
+      num_mmr = 0; num_prearranged = 1
+    when 3
+      num_mmr = 2; num_prearranged = 0
+    when 4
+      num_mmr = 1; num_prearranged = 1
+    when 5
+      num_mmr = 0; num_prearranged = 1
+    when 6
+      num_mmr = 0; num_prearranged = 2
+    end
+    [num_mmr, num_prearranged]
+  end
+
 end
 
