912ba7be74cd99b0faaa1d9d918b36cf2b4785c4
Steve Hanks <hanks@pobox.com>
2015-03-08 12:18:49 -0400

    x

--- app/models/ensemble_primary.rb
+++ app/models/ensemble_primary.rb
@@ -4,4 +4,8 @@ class EnsemblePrimary < ActiveRecord::Base
   has_many :prearranged_chambers
   accepts_nested_attributes_for :mmr_chambers
   accepts_nested_attributes_for :prearranged_chambers
+
+  def default_instrument_id
+    registration.instrument_id
+  end
 end
