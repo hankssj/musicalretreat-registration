c63789ca917972307e895eca1de8c16093527675
Steve Hanks <hanks@pobox.com>
2015-03-08 09:05:51 -0400

    y

--- app/helpers/ensembles_helper.rb
+++ app/helpers/ensembles_helper.rb
@@ -1,22 +1,24 @@
 module EnsemblesHelper
 
-def parse_chamber_music_choice(cmc)
-  num_mmr = num_prearranged = 0
-  case cmc
-  when 0
-    num_mmr = 0; num_prearranged = 0
-  when 1
-    num_mmr = 1; num_prearranged = 0
-  when 2
-    num_mmr = 0; num_prearranged = 1
-  when 3
-    num_mmr = 2; num_prearranged = 0
-  when 4
-    num_mmr = 1; num_prearranged = 1
-  when 5
-    num_mmr = 0; num_prearranged = 1
-  when 6
-    num_mmr = 0; num_prearranged = 2
+  def parse_chamber_music_choice(cmc)
+    num_mmr = num_prearranged = 0
+    case cmc
+    when 0
+      num_mmr = 0; num_prearranged = 0
+    when 1
+      num_mmr = 1; num_prearranged = 0
+    when 2
+      num_mmr = 0; num_prearranged = 1
+    when 3
+      num_mmr = 2; num_prearranged = 0
+    when 4
+      num_mmr = 1; num_prearranged = 1
+    when 5
+      num_mmr = 0; num_prearranged = 1
+    when 6
+      num_mmr = 0; num_prearranged = 2
+    end
+    [num_mmr, num_prearranged]
   end
-  [num_mmr, num_prearranged]
+
 end
