a6d3254a7beed4ec777b6a1febf429314d2c953c
Steve Hanks <hanks@pobox.com>
2015-03-10 15:46:33 -0400

    x

--- db/migrate/20150305233115_create_prearranged_chambers.rb
+++ db/migrate/20150305233115_create_prearranged_chambers.rb
@@ -1,24 +1,13 @@
 class CreatePrearrangedChambers < ActiveRecord::Migration
   def change
     create_table :prearranged_chambers do |t|
-      t.column :ensemble_primary_id, :int
-      t.column :contact_name, :string
-      t.column :string, :string
-      t.column :contact_phone, :string
-      t.column :name_1, :string
-      t.column :instrument_id_1, :int
-      t.column :name_2, :string
-      t.column :instrument_id_2, :int
-      t.column :name_3, :string
-      t.column :instrument_id_3, :int
-      t.column :name_4, :string
-      t.column :instrument_id_4, :int
-      t.column :name_5, :string
-      t.column :instrument_id_5, :int
-      t.column :bring_own_music, :boolean
-      t.column :music_name, :string
-      t.column :music_composer, :string
-
+      t.integer :ensemble_primary_id
+      t.boolean :i_am_contact
+      t.string  :contact_name
+      t.integer :instrument_id
+      t.text    :participant_names
+      t.boolean :bring_own_music
+      t.string  :music_composer_and_name
       t.timestamps
     end
   end
