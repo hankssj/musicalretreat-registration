3ddd3d0564ca63cd99f182ccfa7d0f41c048ff5a
Steve Hanks <hanks@pobox.com>
2015-03-11 01:19:11 -0400

    x

---
+++ app/views/ensembles/_string_evaluation.html.rb
@@ -0,0 +1,10 @@
+<%= render :partial => "contact_info" %>
+
+<%= render :partial => "groups" %>
+<%= render :partial => "practice_frequency" %>
+<%= render :partial => "sightreading_ability" %>
+<%= render :partial => "private_study" %>
+<%= render :partial => "overall_assessment" %>
+<%= render :partial => "jazz_experience" %>
+<%= render :partial => "extra_instruments" %>
+
