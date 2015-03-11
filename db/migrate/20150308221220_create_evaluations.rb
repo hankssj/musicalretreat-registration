4239ba22055897fcc33592e656bdabd75f330cc1
Steve Hanks <hanks@pobox.com>
2015-03-11 11:06:58 -0400

    x

--- db/migrate/20150308221220_create_evaluations.rb
+++ db/migrate/20150308221220_create_evaluations.rb
@@ -10,18 +10,28 @@ class CreateEvaluations < ActiveRecord::Migration
       t.boolean :other_instrument_english_horn
       t.boolean :other_instrument_c_trumpet
       t.boolean :other_instrument_a_clarinet
+      t.boolean :other_instrument_drum_set
+
+      t.boolean :percussion_snare
+      t.boolean :percussion_timpani
+      t.boolean :percussion_mallets
+      t.boolean :percussion_small_instruments
+      t.boolean :percussion_drum_set
 
       t.text :groups
       t.boolean :require_audition
       t.boolean :studying_privately
       t.string :studying_privately_how_long
-      t.integer :chamber_music_how_often   #should be 1 through 3
-      t.integer :practicing_how_much       #should be 1 through 3
+      t.integer :chamber_music_how_often   #should be 0 through 2
+      t.integer :practicing_how_much       #should be 0 through 2
+
+      t.boolean :jazz_small_ensemble
+      t.boolean :jazz_big_band
 
       # Vocal
       t.string :most_difficult_piece
 
-      t.integer :overall_rating_large_ensemble  # should be 1 through 5
+      t.integer :overall_rating_large_ensemble  # should be 0 through 4
       t.integer :overall_rating_chamber
       t.integer :overall_rating_sightreading
 
