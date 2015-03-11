fa4e681f1c3cb2c514dde09b3ce554b0c676744d
Steve Hanks <hanks@pobox.com>
2015-03-11 00:43:59 -0400

    x

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,13 @@
 class Instrument < ActiveRecord::Base
   has_and_belongs_to_many :electives
 
+  def vocal?; instrument_type == "vocal"; end
+  def instrumental?; !vocal?; end
+  def string?; instrument_type == "string"; end
+  def brass?; instrument_type == "brass"; end
+  def woodwind?; instrument_type == "woodwind"; end
+  def percussion?; instrument_type == "percussion"; end
+
   def self.menu_selection
     menu_helper(Instrument.all.reject{|i| i.closed})
   end
@@ -10,14 +17,6 @@ class Instrument < ActiveRecord::Base
     menu_helper(elective.instruments)
   end
 
-  def vocal?
-    instrument_type == "vocal"
-  end
-
-  def instrumental?
-    !vocal?
-  end
-  
   private
   def self.menu_helper(list_of_instruments)
     list_of_instruments.sort{|i1, i2| i1.display_name <=> i2.display_name}.map{|i| [i.display_name, i.id]}
---
+++ app/views/ensembles/evaluations/_percussion_evaluation.html.rb
@@ -0,0 +1,11 @@
+<%= render :partial => "contact" %>
+
+<%= render :partial => "groups" %>
+<%= render :partial => "practice_frequency" %>
+<%= render :partial => "sightreading_ability" %>
+<%= render :partial => "private_study" %>
+<%= render :partial => "overall_assessment" %>
+<%= render :partial => "jazz_experience" %>
+
+<%= render :partial => "extra_instruments" %>
+
