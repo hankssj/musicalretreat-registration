a91385d2cf98811c757c79bcc0c3cdcb9449b86c
Steve Hanks <hanks@pobox.com>
2015-03-14 14:13:41 -0400

    x

--- app/controllers/ensembles_controller.rb
+++ app/controllers/ensembles_controller.rb
@@ -18,7 +18,8 @@ class EnsemblesController < ApplicationController
       redirect_to :controller => :registration, :action => "index"
     end
     @ensemble_primary = EnsemblePrimary.new(:registration_id => @registration.id)
-    @large_ensemble_option = Instrument.find(@registration.instrument_id).large_ensemble
+    @instrument = registration.instrument
+    @large_ensemble_option = @instrument.large_ensemble
     flash[:ensemble_primary_id] = @ensemble_primary.id
   end
 
