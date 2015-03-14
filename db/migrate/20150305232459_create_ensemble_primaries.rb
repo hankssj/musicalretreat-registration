ffea9a53d9d9f76fe11234c6075dcf964c04dea1
Steve Hanks <hanks@pobox.com>
2015-03-14 14:17:08 -0400

    x

--- db/migrate/20150305232459_create_ensemble_primaries.rb
+++ db/migrate/20150305232459_create_ensemble_primaries.rb
@@ -2,10 +2,11 @@ class CreateEnsemblePrimaries < ActiveRecord::Migration
   def change
     create_table :ensemble_primaries do |t|
       t.integer :registration_id
-      t.integer :large_ensemble_choice   # see EnsemblePrimary.enum_name
-      t.integer :large_ensemble_part      # Same as chamber_ensemble_part in Evaluation.
-                                         # 0,1,2 for first second or third. 10 is reserved for piccolo, 
-                                         # 100 - 103 reserved for soprano,alto,tenor,baritone sax
+      t.integer :large_ensemble_choice     # see EnsemblePrimary.enum_name
+      t.integer :chamber_ensemble_choice   # see EnsemblePrimary.enum_name
+      t.integer :large_ensemble_part       # Same as chamber_ensemble_part in Evaluation.
+                                           # 0,1,2 for first second or third. 10 is reserved for piccolo, 
+                                           # 100 - 103 reserved for soprano,alto,tenor,baritone sax
       t.boolean :ack_no_morning_ensemble
       t.boolean :want_sing_in_chorus
       t.boolean :want_percussion_in_band
