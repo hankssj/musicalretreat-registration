b20a71a4e8d7b09ba3440c41d2cfc04497c811c8
Steve Hanks <hanks@pobox.com>
2015-03-16 14:49:33 -0400

    x

--- db/migrate/20150305232459_create_ensemble_primaries.rb
+++ db/migrate/20150305232459_create_ensemble_primaries.rb
@@ -11,6 +11,8 @@ class CreateEnsemblePrimaries < ActiveRecord::Migration
       t.boolean :want_sing_in_chorus
       t.boolean :want_percussion_in_band
 
+      t.text :comments
+
       t.timestamps
     end
   end
