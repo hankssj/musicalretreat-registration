384b15fe93658a9e7fcfb45c138b7d7bc173765e
Steve Hanks <hanks@pobox.com>
2015-05-11 02:16:54 -0400

    eval reminder email

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -162,7 +162,7 @@ class AdminController < ApplicationController
   def send_eval_reminders
     test_emails = ["hanks.steve@gmail.com"]
     #test_emails = nil
-    rr = Registration.where(year: Year.this_year).reject{|r| r.has_complete_eval?}
+    rr = Registration.where(year: Year.this_year).reject{|r| r.has_complete_eval}
     rr = rr.select{|r|test_emails.include?(r.email)} if test_emails
     rr.each{|r| RegistrationMailer.eval_reminder(r)}
     @emails = rr.map{|r|r.email}
