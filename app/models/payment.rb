bab81b02e682c08e728291d81d955d1fda7f752d
Steve Hanks <hanks@pobox.com>
2015-05-13 19:25:58 -0400

    add timestamp to filemaker

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -125,12 +125,12 @@ class Payment < ActiveRecord::Base
     end
 
       ##   To decorate the download page
-    def self.last_download
+    def last_download
       downloads = Download.where(download_type: "payments").order(downloaded_at: :desc)
       downloads.empty? ? Date.new(2000,1,1).to_time : downloads.first.downloaded_at
     end
 
-    def self.num_records_to_download
+    def num_records_to_download
       records_to_send.length
     end
   end
