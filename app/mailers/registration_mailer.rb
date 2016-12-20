e0180f84c5e56c585fedfcd693e7fc3c47eb65ca
Steve Hanks <hanks@pobox.com>
2016-12-20 12:12:33 -0500

    more mass

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -19,7 +19,7 @@ class RegistrationMailer < ActionMailer::Base
   #  This might/should replace the invitation above
   def mass_email_invitation(email, unsubscribe_id)
     @unsubscribe_id = unsubscribe_id
-    attachments.inline['logo.jpg'] = { data: File.read(Rails.root.join('app/assets/images/mmr_logo_email_header.jpg')), mime_type: 'image/jpg'}
+    #attachments.inline['logo.jpg'] = { data: File.read(Rails.root.join('app/assets/images/mmr_logo_email_header.jpg')), mime_type: 'image/jpg'}
     attachments.inline['facebook_icon.jpg'] = { data: File.read(Rails.root.join('app/assets/images/facebook_icon.jpg')), mime_type: 'image/jpg'}
     mail(:from => "online-registration@musicalretreat.org", :to => email, :subject=> "MMR #{Year.this_year} Registration").deliver!
     Event.log("Sent mass invitation email to #{email}")
